0901d8fc337b8ec382064398a7f32b7d7ff04dce
marco-ingenieria <mleanza@fi.uba.ar>
2026-03-21 18:30:07 -0300

    opc4 funcional

--- top10.sh
+++ top10.sh
@@ -3,6 +3,6 @@
 if [ -f ~/EPNro1/salida/"$FILENAME".txt ]; then
     echo "Las notas más altas son:"
     head <(sort -k 5 -nr ~/EPNro1/salida/"$FILENAME".txt) | cut -d " " -f 5
-else;
-    echo "No se encontró el archivo \"$FILENAME\""
+else 
+    echo "No se encontró el archivo \"$FILENAME\"";
 fi
