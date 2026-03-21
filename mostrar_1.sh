ef671c5676aad7e15439c8945117597fe16dbffb
AleCarop <acaropreso@fi.uba.ar>
2026-03-21 20:08:10 -0300

    agrego break para salir del while

--- mostrar_1.sh
+++ mostrar_1.sh
@@ -2,17 +2,19 @@
 
 echo "La variable FILENAME es: $FILENAME"
 
+echo "Ingrese su padron: "
 read padronIngresado
-echo "El padron ingresado es: $padronIngresado"
 
 input=~/"/EPNro1/salida/$FILENAME.txt"
 
-while IFS= read -r line || [ -n "$line" ]
+while IFS= read -r line || [[ -n "$line" ]]
 do
+
     IFS=" "
     read -a array <<< $line
     padronActual="${array[0]}"
     if [[ $padronActual == $padronIngresado ]];then
     echo "$line"
+    break
     fi  
 done < "$input"
