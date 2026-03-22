2337d818602b76e129b90ca4a95340df69400ce3
marco-ingenieria <mleanza@fi.uba.ar>
2026-03-22 14:31:20 -0300

    main llama al resto de scripts

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export FILENAME=$_
+while getopts "d" OPT; do
+    case $OPT in
+    d)
+        bash $(dirname $0)"/borrar_entorno.sh"
+        exit 0
+        ;;
+    esac
+done
 while [ 1 -eq 1 ];
 
 do
@@ -16,32 +25,34 @@ read opcion
 
 case $opcion in
   1)
-     echo "1"
-	mkdir -p ~/EPNro1
-	mkdir -p ~/EPNro1/entrada
-	mkdir -p ~/EPNro1/salida
-	mkdir -p ~/EPNro1/procesado
-     ;;
+    echo "1"
+    mkdir -p ~/EPNro1
+    mkdir -p ~/EPNro1/entrada
+    mkdir -p ~/EPNro1/salida
+    mkdir -p ~/EPNro1/procesado
+    cp data_ejemplo/consolidar.sh ~/EPNro1/
+    cp data_ejemplo/listado*.txt ~/EPNro1/entrada
+    ;;
   2)
-     echo "2"
-	cp data_ejemplo/consolidar.sh ~/EPNro1/
-	bash ~/EPNro1/consolidar.sh &
-     ;;
+    echo "2"
+    bash ~/EPNro1/consolidar.sh &
+    ;;
 
   3)
-     echo "3"
-     ;;
+    echo "3"
+    bash $(dirname $0)"/list_ordenado.sh" 
+    ;;
   4)
-     echo "4"
-     ;;
+    echo "4"
+    bash $(dirname $0)"/top10.sh"
+    ;;
   5)
-     echo "5"
-     ;;
+    echo "5"
+    bash $(dirname $0)"/mostrar_1.sh"
+    ;;
   6)
-     echo "6"
-	exit 0
+    echo "6"
+    exit 0
      ;;
-
 esac
-
 done
